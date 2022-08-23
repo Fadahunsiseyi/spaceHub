a154a6b96755b1e2cfe37fd98a3fb44e121573ad
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-24 00:15:37 +0100

    Change routes to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {
-  Rocket, Mission, Profile, NoPage,
-} from './components';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
 import Layout from './components/shared/Layout';
+import Profile from './pages/Profiles';
+import { NoPage } from './components';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route index exact path="/" element={<Rocket />} />
-          <Route path="/missions" element={<Mission />} />
+          <Route index exact path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
           <Route path="/myprofile" element={<Profile />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
