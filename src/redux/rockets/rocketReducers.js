2207ce3f8c010049adb2f49e132bc158ab880fb8
omarbabou <obabou50@gmail.com>
2022-08-25 01:00:53 +0200

    Add actions creatore to the reducer

--- src/redux/rockets/rocketReducers.js
+++ src/redux/rockets/rocketReducers.js
@@ -11,3 +11,33 @@ const API = 'https://api.spacexdata.com/v3/rockets';
 const IState = {
   rockets: [],
 };
+
+// ACTION CREATORS
+
+export const getRockets = (payload) => ({
+  type: GET_ALL_ROCKETS,
+  payload,
+});
+
+export const createReserve = (payload) => ({
+  type: RESERVEVATION,
+  payload,
+});
+
+export const createJoin = (payload) => ({
+  type: CREATE_JOIN,
+  payload,
+});
+
+export const createReservation = (payload) => ({
+  type: CREATE_RESERVATION,
+  payload,
+});
+
+// ACTIONS
+
+export const getRocketFromApi = () => async (dispatch) => {
+  const request = await fetch(API);
+  const response = await request.json();
+  dispatch(getRockets(response));
+};
