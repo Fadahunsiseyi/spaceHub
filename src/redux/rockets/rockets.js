96079f432afa13fcb8c54dae461b84fb3a2f07c2
omarbabou <obabou50@gmail.com>
2022-08-24 13:24:58 +0200

    Fetch data from GET_ROCKETS

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -10,3 +10,22 @@ const rocketReducer = (state = [], action) => {
       return state;
   }
 };
+
+export const fetchRockets = createAsyncThunk(
+  GET_ROCKETS, async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/rockets');
+    const data = await response.json();
+    const RocketsList = [];
+    data.forEach((rocket) => {
+      RocketsList.push({
+        id: rocket.id,
+        rocket_name: rocket.rocket_name,
+        description: rocket.description,
+        flickr_images: rocket.flickr_images[0],
+      });
+    });
+    return RocketsList;
+  },
+);
+
+export default rocketReducer;
