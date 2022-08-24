11cb3f17bfbd61b7215d8cce104d3acabacdc543
omarbabou <obabou50@gmail.com>
2022-08-25 01:16:50 +0200

    Display RocketList

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRocketFromApi } from '../redux/rockets/rocketReducers';
+import Rock from '../components/Rocket';
 
-function Rockets() {
+const RocketList = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocketReducers);
+
+  useEffect(() => {
+    if (!rockets.rockets[0]) {
+      dispatch(getRocketFromApi());
+    }
+  }, []);
   return (
-    <div>Rockets</div>
+    <div className="rockets">
+      {rockets.rockets.map((rock) => (
+        <Rock key={rock.id} rock={rock} />
+      ))}
+    </div>
   );
-}
+};
 
-export default Rockets;
+export default RocketList;
