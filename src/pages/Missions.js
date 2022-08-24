7f78b235bfa3fb4114751c5bd5cf0b52cdbfbb9a
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-24 03:45:56 +0100

    Add the logic to display mission

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMission } from '../redux/missions/missionReducer';
+import Mission from '../components/Mission';
 
-function Missions() {
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    if (!missions.length) {
+      dispatch(fetchMission());
+    }
+  }, [dispatch]);
+
   return (
-    <div>Missons</div>
+    <div className="mission-container-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <Mission
+              key={mission.mission_id}
+              id={mission.mission_id}
+              name={mission.mission_name}
+              description={mission.description}
+              join={mission.join}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
-}
+};
 
 export default Missions;
