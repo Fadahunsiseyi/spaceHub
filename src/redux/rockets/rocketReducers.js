136e9c1d4ac3423c509dd3c01cd20948ed0c6bcd
omarbabou <obabou50@gmail.com>
2022-08-25 00:59:41 +0200

    Add actions, api and Istate

---
+++ src/redux/rockets/rocketReducers.js
@@ -0,0 +1,13 @@
+// actions
+const GET_ALL_ROCKETS = 'GET_ALL_ROCKETS';
+const RESERVEVATION = 'RESERVEVATION';
+const CREATE_JOIN = 'CREATE_JOIN';
+const CREATE_RESERVATION = 'CREATE_RESERVATION';
+
+// APIS
+const API = 'https://api.spacexdata.com/v3/rockets';
+
+// IState
+const IState = {
+  rockets: [],
+};
