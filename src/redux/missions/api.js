1f566f731642f05643fae85317f16538798bea08
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-24 09:02:11 +0100

    Fix linters error

--- src/redux/missions/api.js
+++ src/redux/missions/api.js
@@ -3,9 +3,7 @@ const url = 'https://api.spacexdata.com/v3/missions';
 export const getMissions = async () => {
   try {
     const result = await fetch(url);
-    console.log(result, 'fetch me');
     const data = await result.json();
-    console.log(data);
     if (result.status === 200) {
       return data;
     }
