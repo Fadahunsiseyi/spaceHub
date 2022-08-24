4cd51f23e3b7f0991250f1ec49e22b3bb6f8622d
omarbabou <obabou50@gmail.com>
2022-08-25 01:02:22 +0200

    Add GET_ALL_ROCKETS, RESERVEVATION and CREATE_RESERVATION functionality

--- src/redux/rockets/rocketReducers.js
+++ src/redux/rockets/rocketReducers.js
@@ -41,3 +41,35 @@ export const getRocketFromApi = () => async (dispatch) => {
   const response = await request.json();
   dispatch(getRockets(response));
 };
+
+// REDUCERS
+
+const reducer = (state = IState, action) => {
+  switch (action.type) {
+    case GET_ALL_ROCKETS:
+      return { ...state, rockets: action.payload };
+    case RESERVEVATION: {
+      const changeState = state.rockets.map((element) => {
+        if (element.id === action.payload.id) {
+          return { ...element, reserve: action.payload.reserve };
+        }
+        return element;
+      });
+      return { ...state, rockets: changeState };
+    }
+    case CREATE_RESERVATION: {
+      const changeState = state.dragons.map((element) => {
+        if (element.id === action.payload.id) {
+          return { ...element, reservation: action.payload.reservation };
+        }
+        return element;
+      });
+      return { ...state, dragons: changeState };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
