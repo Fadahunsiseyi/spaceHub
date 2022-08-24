1d4967849430fe5067c8a50cae6f886c5501aede
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-24 03:14:46 +0100

    Add logic to join mission

--- src/redux/missions/missionReducer.js
+++ src/redux/missions/missionReducer.js
@@ -4,13 +4,24 @@ const JOIN_MISSION = ' JOIN_MISSION';
 const LEAVE_MISSION = ' LEAVE_MISSION';
 const FETCH_MISSION = ' FETCH_MISSION';
 
-const initialState = []
-export default function (state=initialState, action) {
-    switch(action.type) {
-        case FETCH_MISSION:
-            return action.payload.map((mission) => ({
-                ...mission,
-                join: false
-            }))
-    }
+const initialState = [];
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_MISSION:
+      return action.payload.map((mission) => ({
+        ...mission,
+        join: false,
+      }));
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.id === action.payload.id) {
+          return {
+            ...state,
+            join: true,
+          };
+        }
+      });
+    default:
+      return state;
+  }
 }
