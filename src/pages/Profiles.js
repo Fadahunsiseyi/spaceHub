7aea6e4f45fbbf0391074e71bc52e79a6096e703
omarbabou <obabou50@gmail.com>
2022-08-25 14:55:47 +0200

    Add Myprofile interactivity with rockets and missions

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Profiles() {
+const Myprofile = () => {
+  const GState = useSelector((state) => state.rocketReducers);
+  const resItem = GState.rockets.filter((item) => item.reserve === true);
+  /* const misItem = GState.missions.filter((item) => item.join === true); */
+
   return (
-    <div>Profile</div>
+    <div className="wrapper_profile">
+      <div className="profile">
+        <h3>My Rockets</h3>
+        <ul className="list">
+          {resItem.map((rock) => <li key={rock.rocket_name}>{rock.rocket_name}</li>)}
+        </ul>
+      </div>
+      <div className="profile">
+        <h3>My Missions</h3>
+        {/* <ul className="list">
+          {
+            misItem.map((mission) => (
+              <li key={mission.mission_name}>{mission.mission_name}</li>
+            ))
+          }
+        </ul> */}
+      </div>
+    </div>
   );
-}
+};
 
-export default Profiles;
+export default Myprofile;
