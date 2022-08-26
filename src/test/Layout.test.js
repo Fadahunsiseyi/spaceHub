8ccb7414431fb575c1a8c1c4acb899c5c903a489
omarbabou <obabou50@gmail.com>
2022-08-26 12:48:06 +0200

    Add tests for the NavBar components

---
+++ src/test/Layout.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/ConfigureStore';
+import Layout from '../components/shared/Layout';
+
+describe('Pages renders correctly', () => {
+  it('Should redenders NavBar Component succefully', () => {
+    const app = renderer
+      .create(
+        <Provider store={store}>
+          <Router>
+            <Layout />
+          </Router>
+        </Provider>,
+      )
+      .toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
